9ea1fc0c8400efea36e8170ba18f56b34b2e67bf
Justin-Seaman <57916858+Justin-Seaman@users.noreply.github.com>
2020-03-06 12:23:56 -0500

    Add files via upload

---
+++ PSFileNameEdit.ps1
@@ -0,0 +1,17 @@
+﻿$Folder = Read-Host -Prompt 'Input the path of the folder with ONLY files you would like to rename'
+$Head = Read-Host -Prompt 'Input what to append to the BEGINING of each file'
+$Tail = Read-Host -Prompt 'Input what to apped to the END of each file'
+$StartIndex = Read-Host -Prompt 'Input the index number of substring BEGINING (Input 0 if no substring desired)'
+$EndIndex = Read-Host -Prompt 'Input the index number of substring ENDING (Input 0 if no substring desired)'
+
+
+cd $Folder
+If ($StartIndex -ge $EndIndex){
+    dir |Rename-Item -NewName {$Head + $_.name.substring($StartIndex) + $Tail}
+}Else{
+    dir |Rename-Item -NewName {$Head + $_.name.substring($StartIndex,$EndIndex) + $Tail}
+}
+
+
+
+
